8119cf9e7d1251d3b294c532ee600abfcdb1e9fc
ayaniegain <ayaniegain@gmail.com>
2024-06-01 09:34:32 +0530

    Update multer and cloidenery

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json({limit:'16kb'}))
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 app.use(cookieParser())
  
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,18 +8,13 @@ import { ApiResponse } from "../utils/apiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, username, email, password } = req.body;
 
-  console.log("fullName", fullName);
-  console.log("username", username);
-  console.log("email", email);
-  console.log("password", password);
-
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All field are required");
   }
 
-  let existingUser = User.findOne({
+  let existingUser =await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -29,14 +24,14 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const avatarlocalPath = req.files?.avatar[0].path;
   const coverImagelocalPath = req.files?.coverImage[0].path;
-
+  
   if (!avatarlocalPath) {
     throw new ApiError(409, "avatar localpath required ");
   }
-
+  
   const avatar = await uploadOnCloudenary(avatarlocalPath);
   const coverImage = await uploadOnCloudenary(coverImagelocalPath);
-
+  
   if (!avatar) {
     throw new ApiError(409, "avatar required ");
   }
@@ -58,14 +53,12 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "something went wrong while new user create ");
 
    }
-
    return res.status(201).json(
-      new ApiResponse(200,createdUser,"user registered successfully")
+      new ApiResponse(201,createdUser,"user registered successfully")
    )
 
 
 
-  res.send("okey");
 });
 
 export { registerUser };
